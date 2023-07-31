98ec6563332a599d92e56b3b8f350a1e726682b2
Kris Kurzawa <kurzawa1@yahoo.com>
2023-07-30 20:34:31 -0400

    bt 1

--- READMESAMPLE.md
+++ READMESAMPLE.md
@@ -1,4 +1,4 @@
-[![License: CC BY 1.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/1.0/)
+
 # f
 
 ## Description
@@ -37,5 +37,5 @@ f
 f
 
 ## License
-Creative Commons Zero v1.0 Universal
+None
    
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,18 +18,18 @@ function renderLicenseLink(data) {
         yourLincense = "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
     } else if (licenseType === 'BSD 3-Clause "New" or "Revised"') {
         yourLincense = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-        // } else if (licenseType === 'Boost Software 1.0') {
-        //     yourLincense = 
+    } else if (licenseType === 'Boost Software 1.0') {
+        yourLincense = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
     } else if (licenseType === 'Creative Commons Zero v1.0 Universal') {
-        yourLincense = "[![License: CC BY 1.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/1.0/)"
+        yourLincense = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"
     } else if (licenseType === 'Eclipse Public 2.0') {
-        yourLincense = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-2.0)"
+        yourLincense = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
     } else if (licenseType === 'GNU Affero General Public v3.0') {
         yourLincense = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
     } else if (licenseType === 'GNU General Public v2.0') {
         yourLincense = "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
     } else if (licenseType === 'GNU Lesser General Public v2.1') {
-        yourLincense = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-2.1)"
+        yourLincense = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
     } else if (licenseType === 'Mozilla Public 2.0') {
         yourLincense = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
     } else if (licenseType === 'The Unlicense') {
